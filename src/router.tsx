522abb13ca201c82b6b184ff71edbf1cc237315e
mauricio.rhbrasnica <mauricio.rhbrasnica@gmail.com>
2026-05-25 15:22:39 +0000

    fix: ativa roteamento por hash manual para corrigir erro de refresh

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,16 @@
 import { QueryClient } from "@tanstack/react-query";
-import { createRouter } from "@tanstack/react-router";
+import { createRouter, createHashHistory } from "@tanstack/react-router"; // Adicionado createHashHistory
 import { routeTree } from "./routeTree.gen";
+
+// Cria o histórico baseado em Hash de forma manual
+const hashHistory = createHashHistory();
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
 
   const router = createRouter({
     routeTree,
+    history: hashHistory, // Força o roteador a usar o modo hash seguro (#/)
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
